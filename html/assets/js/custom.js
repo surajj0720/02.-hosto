ee55558cc288717ebfeaca6bfaf90c46f121ec91
surajj0720 <suraj.dutech@gmail.com>
2023-04-18 12:05:53 +0530

    testi

--- html/assets/js/custom.js
+++ html/assets/js/custom.js
@@ -56,6 +56,34 @@ Version: 1.0.0
         });
 
         // filter end
+
+
+        $('.ht_testimonial_slider').slick({
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 3,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+
     });
 })();
 
